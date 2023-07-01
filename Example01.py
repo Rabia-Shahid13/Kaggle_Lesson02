58648b39d662873fb46a72014cbd399d0bb53092
Rabia-Shahid13 <138299971+Rabia-Shahid13@users.noreply.github.com>
2023-07-01 20:12:38 +0500

    Add files via upload

---
+++ Example01.py
@@ -0,0 +1,9 @@
+"""Return the given number rounded to two decimal places.  
+    >>> round_to_two_places(3.14159)
+    3.14 """
+
+num = 3.14159
+
+def round_to_two_places(num):
+    
+    return round(num, 2)
---
+++ Example04.py
@@ -0,0 +1,8 @@
+# ruound_to_two_places(9.9999)
+rounded_no = round(9.999, 2)
+print("Rounded no upto 2 decimal places is:", rounded_no)
+
+# Which of the two variables above has the smallest absolute value?
+x = -10
+y = 5
+print("Smallest value is:", min(x,y))
